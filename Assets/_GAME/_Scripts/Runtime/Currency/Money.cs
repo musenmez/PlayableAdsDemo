83e9d7764cb3712fbc113127ec9cf4b0c7089137
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2025-11-25 18:16:46 +0300

    Add money collection animation

--- Assets/_GAME/_Scripts/Runtime/Currency/Money.cs
+++ Assets/_GAME/_Scripts/Runtime/Currency/Money.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Game.Runtime
@@ -7,6 +8,8 @@ namespace Game.Runtime
     public class Money : MonoBehaviour, IInteractable
     {
         public bool IsInteractable { get; private set; }
+
+        private const float COLLECT_OFFSET = 2f;
 
         private int _value;
         private IMoneyStack _stack;
@@ -16,19 +19,33 @@ namespace Game.Runtime
             IsInteractable = true;
             _value = value;
             _stack = stack;
+            transform.DOPunchScale(Vector3.one * 0.3f, 0.2f).SetEase(Ease.InOutSine);
         }
 
         public void Interact(Interactor interactor)
         {
+            if (!IsInteractable)
+                return;
+            
             IsInteractable = false;
             _stack?.RemoveFromStack(this);
-            CollectTween();
+            CollectTween(interactor.transform);
         }
 
-        private void CollectTween()
+        private void CollectTween(Transform target)
         {
-            CurrencyManager.Instance.AddCurrency(_value);
-            gameObject.SetActive(false);
+            transform.DOComplete();
+            transform.SetParent(target);
+            var sequence = DOTween.Sequence();
+            sequence.Join(transform.DOScale(0.75f, 0.2f).SetEase(Ease.Linear))
+                .Join(transform.DOLocalRotateQuaternion(Random.rotation, 0.2f).SetEase(Ease.Linear))
+                .Join(transform.DOLocalJump(Vector3.up * COLLECT_OFFSET, 2f, 1, 0.2f).SetEase(Ease.Linear))
+                .OnComplete(() => 
+                {
+                    transform.SetParent(null);
+                    gameObject.SetActive(false);
+                    CurrencyManager.Instance.AddCurrency(_value);
+                });
         }
 
         public void InteractorExit(Interactor interactor){}
