1f9510eb2e4e4432090533ca8eb6ba6ca3b32838
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2025-11-26 15:26:33 +0300

    Fix money prefab

--- Assets/_GAME/_Scripts/Runtime/GameStates/InitialState.cs
+++ Assets/_GAME/_Scripts/Runtime/GameStates/InitialState.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Game.Runtime
 {
     public class InitialState : GameStateBase
     {
+        private Coroutine _initialCo;
+        
+        private const float INITIAL_DELAY = 0.2f;
+        
         public override void Enter()
+        {
+            StopCoroutine(_initialCo);
+            _initialCo = GameManager.Instance.StartCoroutine(InitializeCo());
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            if (_initialCo != null)
+                StopCoroutine(_initialCo);
+        }
+
+        private IEnumerator InitializeCo()
         {
             PoolingManager.Instance.Initialize();
             CurrencyManager.Instance.Initialize();
             TaskManager.Instance.Initialize();
-            GameManager.Instance.SetState(GameStateId.InGame);
+            yield return new WaitForSeconds(INITIAL_DELAY);
+            DOVirtual.DelayedCall(0.1f, () => GameManager.Instance.SetState(GameStateId.InGame));
         }
     }
 }
