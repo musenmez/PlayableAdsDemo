7569f3045f03ee4e1a472c92c6ec6c6d3f1c8df0
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2025-11-26 12:40:11 +0300

    Convert animations to Generic

--- Assets/_GAME/_Scripts/Runtime/Passenger/PassengerAnimator.cs
+++ Assets/_GAME/_Scripts/Runtime/Passenger/PassengerAnimator.cs
@@ -14,6 +14,10 @@ namespace Game.Runtime
         public const int CARRY_LAYER = 1;
         
         [SerializeField] private Animator animator;
+        
+        private const float DEFAULT_WALK_SPEED = 1f;
+        private const float CARRY_WALK_SPEED = 0.5f;
+        private const float CARRY_SPEED_THRESHOLD = 0.2f;
 
         private Tween _layerTween;
         private Vector3 _oldPosition;
@@ -37,6 +41,20 @@ namespace Game.Runtime
         {
             _layerTween.Kill();
             _layerTween = DOVirtual.Float(animator.GetLayerWeight(layerIndex), weight, duration: duration, (x) => animator.SetLayerWeight(layerIndex, x));
+            CheckWalkSpeed(layerIndex, weight);
+        }
+        
+        private void SetFloat(int hash, float value)
+        {
+            animator.SetFloat(hash, value);
+        }
+
+        private void CheckWalkSpeed(int layerIndex, float weight)
+        {
+            if (layerIndex != CARRY_LAYER)
+                return;
+            
+            SetFloat(AnimationHashes.WalkSpeedMultiplier, weight > CARRY_SPEED_THRESHOLD ? CARRY_WALK_SPEED : DEFAULT_WALK_SPEED);
         }
         
         private void SetSpeed()
